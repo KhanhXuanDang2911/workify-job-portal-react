acbb09a0aec1ebe615c8c74125e5d2dbd9e10df0
Dung Văn <dungvan.170204@gmail.com>
2025-10-14 16:16:28 +0700

    feat: add services for managing provinces and districts

---
+++ src/types/location.type.ts
@@ -0,0 +1,13 @@
+export interface Province {
+  id: number;
+  name: string;
+  engName: string;
+  code: string;
+}
+
+export interface District {
+  id: number;
+  name: string;
+  code: string;
+//   province: Province;
+}
